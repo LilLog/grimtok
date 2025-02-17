ed58d94ce5fdd9c32cadf674a0a020940508c7fb
LilLog <gou2wa@hotmail.com>
2025-02-16 18:25:22 -0800

    add title

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ const App: React.FC = () => {
 
   return (
     <div className="min-h-screen bg-white">
+      <h1 className="fixed top-4 left-4 text-3xl font-bold text-gray-300 p-2 z-50">GrimTok</h1>
       <SnapScrollComponent />
     </div>
   );
